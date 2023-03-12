3e7f8137deab3c6b14d8c3da2d931c2a4a159c35
martinjup <97602564+martinjup@users.noreply.github.com>
2023-03-12 11:05:08 -0400

    New Routes for Login

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Home } from './views/Home/Home'
+import { Login } from './views/Login/Login'
 import { Register } from './views/Register/Register'
 import {MovieDetails} from './views/MovieDetails/MovieDetails'
 import {MoviesList} from './views/MovieList/MovieList'
@@ -13,7 +14,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/home" element={<Home />} />
         <Route exact path='/register' element={<Register />} />
         <Route exact path="/movies/:movieId" element={<MovieDetails />}/>
         <Route exact path="/moviesList" element={<MoviesList />}/>
