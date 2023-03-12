d937f4b2da26e39b12da3953e9c80b4a22ceb17f
Abrahan1820 <104142023+Abrahan1820@users.noreply.github.com>
2023-03-12 19:03:05 -0400

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,26 @@
 import './Navbar.css';
 import { Link } from 'react-router-dom';
 import Search from "../../components/Search/Search"
+import { logout } from '../../firebase/auth-service';
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar(){
+    const navigate = useNavigate()
+    const handleLogout  = () => {
+        logout()
+          
+        .then(() => {
+          navigate('/register')
+            alert('Logout exitoso')
+        })
+        
+        .catch(error)
+         alert('Error en autenticacion, intente de nuevo')
+        };
+
     return(
         <div id='Navbar'>
-            <Link id='logoutButton'>Cerrar Sesion</Link>
+            <Link id='logoutButton' onClick={handleLogout}>Cerrar Sesion</Link>
             <Link id='start' to='/home' className='link'>Inicio</Link>
             <Search/>
         </div>
